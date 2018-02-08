14b0491a8d6435e87c2d4a1afa64194df41a7932
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-08 19:33:29 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.select do |key, value|
+  array = []
+  name_hash.each do |key, value|
     return key if value  
     end 
 end
